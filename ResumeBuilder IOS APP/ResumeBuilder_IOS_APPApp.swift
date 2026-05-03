f517cf5db217c6f087940021b5bf7f25cca562b4
Nadav Yigal <nadav@runsmart.dev>
2026-05-03 15:44:45 +0300

    Initial Commit

---
+++ ResumeBuilder IOS APP/ResumeBuilder_IOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeBuilder_IOS_APPApp.swift
+//  ResumeBuilder IOS APP
+//
+//  Created by Nadav Yigal on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ResumeBuilder_IOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
